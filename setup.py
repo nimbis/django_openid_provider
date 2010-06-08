3c285ae131575ce4a57c32a90e87a20e781e0f57
Bruno Renié <buburno@gmail.com>
2010-06-08 20:33:45 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "django_openid_provider",
+    version = "0.2",
+    author = "Roman Barczyński",
+    description = "An OpenID provider for your django.contrib.auth accounts",
+    long_description = open("README.txt").read(),
+    license = "Apache",
+    url = "http://www.romke.net/django/openid_provider/",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
