8e5d1f011f4414045a32e0fe9b5eb737f8ffdc62
Jannis Leidel <jannis@leidel.info>
2011-03-31 14:42:26 +0200

    It's one of those days.

--- openid_provider/conf.py
+++ openid_provider/conf.py
@@ -14,4 +14,4 @@ SREG_DATA_CALLBACK = getattr(settings, 'OPENID_PROVIDER_SREG_DATA_CALLBACK',
                              'openid_provider.utils.get_default_sreg_data')
 
 AX_DATA_CALLBACK = getattr(settings, 'OPENID_PROVIDER_AX_DATA_CALLBACK',
-                           'openid_provder.utils.get_default_ax_data')
+                           'openid_provider.utils.get_default_ax_data')
