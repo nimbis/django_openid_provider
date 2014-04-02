eee660657ccff646b4a8e45937310bf9133295bc
Brian Schott <brian.schott@nimbisservices.com>
2014-04-02 12:06:22 -0400

    fix urls.py for django > 1.4

--- openid_provider/urls.py
+++ openid_provider/urls.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4 sw=4 : */
 
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # Django < 1.4
+    from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('openid_provider.views',
+urlpatterns = patterns(
+    'openid_provider.views',
     url(r'^$', 'openid_server', name='openid-provider-root'),
     url(r'^decide/$', 'openid_decide', name='openid-provider-decide'),
     url(r'^xrds/$', 'openid_xrds', name='openid-provider-xrds'),
-    url(r'^(?P<id>.*)/$', 'openid_xrds', {'identity': True}, name='openid-provider-identity'),
+    url(r'^(?P<id>.*)/$', 'openid_xrds',
+        {'identity': True}, name='openid-provider-identity'),
 )
